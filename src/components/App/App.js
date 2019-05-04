f1a6dad183b03b90e775c5b8cb57faecfafe696e
meowmere <miaumere@gmail.com>
2019-05-04 12:43:48 +0200

    Deleted experimental Form component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.scss';
-import Form from '../Form/Form';
 import CharactersList from '../CharactersList/CharactersList'
 
 class App extends React.Component{
@@ -10,17 +9,9 @@ class App extends React.Component{
   return (
     <div className="App" >
     <CharactersList />
-    <br />
-    <br />
-    <Form />
     </div>
   );
   }
 }
 
 export default App;
-
-// Nowy komponent o nazwie CharactersList 
-// Gdy komponent zostanie zinicjalizowany, ma to zostac pobrane zostac wywolany rest - lista postaci 
-// index.php z listą postaci, JSON  to ma byc
-// + obsługa błędów
