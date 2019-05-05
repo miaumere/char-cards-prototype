c65621eac41e71354f7a036d5a9dfb0c4e40dc01
meowmere <miaumere@gmail.com>
2019-05-05 20:07:20 +0200

    Small CSS changes, added Appearance component

---
+++ src/components/Info/Appearance/Appearance.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./Appearance.scss";
+
+function Appearance(){
+
+    return(
+
+        <div className="Appearance">
+            <h2 className="appearance appearance__title">Wygląd</h2>
+            <p className="appearance appearance__desc">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies eu nulla id lacinia. M
+            auris vulputate vel ante vitae dapibus. Morbi dapibus, purus sed iaculis pulvinar, est velit rho
+            ncus elit, nec dapibus neque sem ac metus. Maecenas sit amet ligula vestibulum, euismod quam non
+            , lobortis urna. Donec varius fringilla leo, venenatis tempus massa porta at. Nulla ac libero vive
+            rra, sagittis leo in, dictum orci. Fusce fringilla, nunc sed porttitor placerat, sem elit molestie sa
+            pien, in elementum massa magna id risus. In placerat euismod lacus et mattis. Ut pretium fringilla m
+            i molestie ultrices. Phasellus rhoncus vestibulum suscipit. Nulla elit leo, convallis ac iaculis eget
+            , porta vitae sem.
+
+            Nulla placerat purus id mauris molestie pellentesque. Quisque ut tincidunt nulla. Cras convallis
+                ut lorem vitae vulputate. Aliquam a pellentesque nulla. Nam suscipit gravida vulputate. Aliquam dictum 
+                auctor dolor. Integer quis lorem egestas ante porta congue ac non orci. Morbi sit amet mi ut lorem viver
+             ra sodales sit amet eu neque.
+
+           tiam nec suscipit ipsum. Praesent condimentum orci non arcu malesuada, ut consectetur tellus blandit. Nullam et consectetur purus, sit amet auctor tellus. Proin maximus dui id est pharetra, quis ultricies ex ullamcorper. Sed lacinia ut odio non gravida. Suspendisse vulputate, eros ut vestibulum placerat, ante ex iaculis massa, eu porttitor neque ipsum vitae mi. Nam elementum commodo ex, vitae gravida mauris. Pellentesque sit amet commodo felis. Pellentesque porta nec velit in rhoncus. Mauris bibendum purus arcu, nec commodo nulla tempus vitae. Aliquam dolor diam, sagittis eu blandit nec, dapibus id nisi.
+            </p>
+        </div>
+    )
+}
+export default Appearance
--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Info.scss";
 import Temperament from './Temperament/Temperament';
 import PersonImages from './PersonImages/PersonImages';
+import Appearance from './Appearance/Appearance';
 
 function Info() {
     return (
@@ -10,18 +11,21 @@ function Info() {
             <PersonImages />
 
             <div className="desc">
-                <h1 className="name">Jean de Valette</h1>
-                <div>
-                    DATA URODZENIA: xx.xx.1970
+                <h1 className="desc__name">Jean de Valette</h1>
+                <div clasName="desc__other">
+                    Data urodzenia: xx.xx.1970
                     <br />
-                    STATUS: żyjący
+                    Status: żyjący
                     <br />
-                    OSOBOWOŚĆ MBTI: INTJ
+                    Osobowość MBTI: INTJ
+                    <br />
+                    
                 </div>
             </div>
 
             <Temperament sanguine="50" choleric="30" flegmatic="70" melancholic="90" />
 
+            <Appearance />
         </section>
     )
 }
